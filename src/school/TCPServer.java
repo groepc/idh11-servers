e6ca6bb20bc0df943be96e88e7ba51b7aed9c006
Perry Faro <perryfaro@live.nl>
2016-02-28 20:58:32 +0100

    Initial commit

---
+++ src/school/TCPServer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school;
+
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+
+    public static void main(String argv[]) throws Exception {
+        String clientSentence;
+        String capitalizedSentence;
+
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while (true) {
+
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient
+                    = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient
+                    = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine();
+
+            capitalizedSentence = clientSentence.toUpperCase() + " Dit ging goed\n";
+
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
+}
